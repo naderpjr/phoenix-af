845f329d3c6fcb3b226fa930c83a9b31e9911644
Nader PJR <pjrnader@gmail.com>
2025-12-14 20:42:14 +0430

    second commit

---
+++ main.js
@@ -0,0 +1,42 @@
+
+function startLoader() {
+    let counterElement = document.querySelector('.count p');
+    let currentValue = 0;
+
+    function updateCounter() {
+        if (currentValue < 100) {
+            let increment = Math.floor(Math.random() * 10) + 1;
+            currentValue = Math.min(currentValue + increment, 100);
+            counterElement.textContent = currentValue;
+
+            let delay = Math.floor(Math.random() * 200) + 25;
+            setTimeout(updateCounter, delay);
+        }
+    }
+
+    updateCounter();
+
+}
+
+startLoader();
+gsap.to(".count", { opacity: 0, delay: 3.5, duration: 0.5 });
+
+let textWrapper = document.querySelector(".ml16");
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.ml16 .letter',
+        translateY: [-100, 0],
+        easing: "easeOutExpo",
+        duration: 1500,
+        delay: (el, i) => 30 * i
+    })
+    .add({
+        targets: '.ml16 .letter',
+        translateY: [0, 100],
+        easing: "easeOutExpo",
+        duration: 3000,
+        delay: (el, i) => 2000 + 30 * i
+    })
